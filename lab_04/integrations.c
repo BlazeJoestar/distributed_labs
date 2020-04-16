ba18909b42ade1a69e86bdf5f025955f5aefcc24
Blazej Cwiklinski <blazej.cwiklinski.98@gmail.com>
2020-04-16 04:51:08 +0200

    made it work - multiprocessing on windows

--- lab_04/integrations.c
+++ lab_04/integrations.c
@@ -3,48 +3,110 @@
 #include <windows.h>
 #include <conio.h>
 
+#ifndef M_PI
+#    define M_PI 3.14159265358979323846
+#endif
+
 #define STEP 0.001
 #define TO_RADIAN(a) a * (M_PI/180)  
 
 const double step = 0.0001;
-double to_radian(double a){ return a * M_PI / 180.0; }
+double to_radian(double a) { return a * M_PI / 180.0; }
+
+typedef struct pair {
+	double beginning;
+	double ending;
+	double area;
+} pair_t;
 
 // calculates aproximate area under the figure
-double sin_integration(double a, double b){
+DWORD WINAPI sin_integration(void* args) {
+	pair_t* temp = (pair_t*)args;
+	double a = temp->beginning;
+	double b = temp->ending;
 	double sum = 0.0;
-	for(double i = a; i < b; i+=step){
-		// STEP == HEIGHT --> WE CALCULATE THE AREA OF TRAPEZIOD
-		//if ((i > 0.0 && i+step > 0.0) || (i < 0.0 && i+step < 0.0)) {
-			sum += ( (step/2) * (sin(i) + sin(i+step)) );
-	//	}
+	for (double i = a; i < b; i += step) {
+		sum += ((step / 2) * (sin(i) + sin(i + step)));
 	}
-	return sum; 
+	temp->area = sum;
+	return 0;
+}
+pair_t* prepare_intervals(double a, double b, int intervals) {
+	pair_t* arr = (pair_t*)malloc(sizeof(pair_t) * intervals);
+	double temp = a, difference = (b - a) / intervals;
+	for (int i = 0; i < intervals; i++, temp += difference) {
+		arr[i].beginning = temp;
+		arr[i].ending = temp + difference;
+	}
+	return arr;
 }
 
-HANDLE viper_threads[4];
+HANDLE* viper_threads = NULL;
 
 int main(int ac, char* av[]) {
 
 	double beg = 0.0, end = 0.0;
 	int threads = 1;
-	if (ac < 4){
+	if (ac < 4) {
 		printf("please, provide:\n beginning of the interval (in radians): ");
 		scanf("%lf", &beg);
 		printf("please, provide:\n ending of the interval (in radians): ");
 		scanf("%lf", &end);
-		printf("please, provide:\n number of threads: ");
+		printf("please, provide:\n number of threads (1 - 4): ");
 		scanf("%d", &threads);
+		if (threads > 4) {
+			threads = 4;
+		}
+		else if (threads < 1) {
+			threads = 1;
+		}
+
 	}
 	else {
 		beg = atol(av[1]);
 		end = atol(av[2]);
 		threads = atoi(av[3]);
 	}
-	double result = sin_integration(beg, end);
-	printf("calculated integral of sin() for interval <%lf, %lf> (radians) calculated by %d proces%s\n result: %lf\n", beg, end, threads, (threads==1)? " " : "ses", result);
-	
-	//printf("\n %lf \n", sin(TO_RADIAN(end)));
-	
+	//double result = sin_integration(beg, end);
+	//printf("calculated integral of sin() for interval <%lf, %lf> (radians) calculated by %d proces%s\n result: %lf\n", beg, end, threads, (threads == 1) ? " " : "ses", result);
+
+	pair_t* intervals = prepare_intervals(beg, end, threads);
+
+	viper_threads = (HANDLE*)malloc(sizeof(HANDLE) * threads);
+	DWORD thread_id;
+
+	for (int i = 0; i < threads; i++) {
+		viper_threads[i] = CreateThread(
+			NULL,
+			0,
+			sin_integration,
+			(void*)&intervals[i],
+			0,
+			&thread_id
+			);
+	}
+	DWORD synchro = WaitForMultipleObjects(
+		threads,
+		viper_threads,
+		TRUE,
+		INFINITE
+		);
+
+	double result = 0.0;
+
+	for (int i = 0; i < threads; i++) {
+		result += intervals[i].area;
+	}
+
+	printf("calculated integral of sin() for interval <%lf, %lf> (radians) calculated by %d proces%s\n result: %lf\n",
+		beg, end, threads, (threads == 1) ? " " : "ses", result);
+
+	for (int i = 0; i < threads; i++) {
+		CloseHandle(viper_threads[i]);
+	}
+
+	free(intervals);
+	free(viper_threads);
 	return 0;
 }
 
